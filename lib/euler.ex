2a105427f3857fb89b0c0fb14c9390b67283b759
Ian Atha <ian@mamabear.io>
2019-04-03 09:35:17 -0700

    Problem 6

--- lib/euler.ex
+++ lib/euler.ex
@@ -104,4 +104,23 @@ defmodule Euler do
       _ -> sol_5(n+1, divisors, divisors)
     end
   end
+
+
+  @doc """
+  The sum of the squares of the first ten natural numbers is,
+    1^2 + 2^2 + ... + 10^2 = 385
+  The square of the sum of the first ten natural numbers is,
+    (1 + 2 + ... + 10)^2 = 55^2 = 3025
+  Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+  Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+  ## Example
+        iex> Euler.sol_6()
+        25_164_150
+  """
+  def sol_6 do
+    nums = Enum.to_list 1..100
+    :lists.sum(nums)*:lists.sum(nums) - :lists.sum(Enum.map(nums, &(&1*&1)))
+  end
 end
